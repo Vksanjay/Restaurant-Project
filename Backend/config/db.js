6c2644ee18f2727fdbea8b46c76db4b2f49dacf9
Sanjay <vksanjay1107@gmail.com>
2026-09-08 15:34:22 +0530

    dbname updated

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,8 @@ const mongoose=require("mongoose");
 const data=async()=>{
     try {
         const conn=await mongoose.connect(process.env.Mongo_URI);
-        console.log(`Mongodb connected : ${conn.connection.host} `)
+        console.log(`Mongodb connected : ${conn.connection.host} `);
+        console.log(`Database: ${conn.connection.name}`);
     } catch (error) {
         console.error(`Error : ${error.message}`);
         process.exit(1);
