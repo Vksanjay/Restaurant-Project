0ad1679e0e8733f420479a67f9419ef13a6db241
Sanjay <vksanjay1107@gmail.com>
2026-09-08 19:29:29 +0530

    updated server.js

--- Backend/server.js
+++ Backend/server.js
@@ -49,11 +49,26 @@ app.use((err, req, res, next) => {
     });
 });
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log(
-        `Server is running on port ${process.env.PORT || 5000}`
-    );
-});
+// app.listen(process.env.PORT || 5000, () => {
+//     console.log(
+//         `Server is running on port ${process.env.PORT || 5000}`
+//     );
+// });
+const startServer = async () => {
+    try {
+        await db();
+
+        app.listen(process.env.PORT || 5000, () => {
+            console.log(
+                `Server is running on port ${process.env.PORT || 5000}`
+            );
+        });
+    } catch (error) {
+        console.error("Database connection failed:", error.message);
+    }
+};
+
+startServer();
 
 
 
