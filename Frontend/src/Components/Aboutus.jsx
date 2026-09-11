1f1eeae3eb29a9827544693c3d0cfb7411f67f7f
Sanjay <vksanjay1107@gmail.com>
2026-09-11 13:06:56 +0530

    improve responsive

--- Frontend/src/Components/Aboutus.jsx
+++ Frontend/src/Components/Aboutus.jsx
@@ -24,7 +24,7 @@ function Aboutus() {
             <div className="box">
               <h2>🍴</h2>
               <h4 className="fon7">Fresh Ingredients</h4>
-              <p className="fon6">We use a fresh and quality ingredients in every dish.</p>
+              <p className="fon6">We use the fresh and quality ingredients in every dishes.</p>
             </div>
           </div>
           <div className="col-md-4 mb-3">
--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
                 <div className="row">
                     <div className="col-12 col-md-8 col-lg-6">
                         <h1 className="home-title"><i>Food Heaven</i></h1>
-                        <p className="mt-4 home-description">Welcome to Food Heaven, where every meal is made with passion and served with love. Enjoy delicious flavors, fresh ingredients, and a warm dining experience that makes every visit special. From mouth-watering biryanis to delightful desserts, we bring you food that turns every meal into a memorable experience.</p>
+                        <p className="mt-4 home-description">Welcome to Food Heaven, where every meal is made with passion and served with love. Enjoy delicious flavors, fresh ingredients, and a warm dining experience that makes every visit special. From mouth-watering biryanis to a delightful desserts, we bring you food that turns every meal into a memorable experience.</p>
                     </div>
                 </div>
             </div>
